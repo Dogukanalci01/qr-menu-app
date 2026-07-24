354e662358409c95ca15276d7902e3ef03ced7d4
Dogukanalci01 <dogukannalci01@gmail.com>
2026-07-24 11:00:27 +0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://gkvxjwovezmfpmpbhra.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdrdnhqd292ZXh6bWZwbXBiaHJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4NzQwODcsImV4cCI6MjEwMDQ1MDA4N30.oroycUrNPDDc8S-aNKhMLVjHfL3gveyq4pDg32dKp5o';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
